1a6f3228bb758217b18f6373cd9020e0c3be18c6
Jens Nilsson <jens@jensnilsson.nu>
2016-03-14 21:23:00 +0100

    working, panel listing all monitors for the current api-key with status-indicators. Using the settings-API no api-key is provided via config.

--- index.js
+++ index.js
@@ -1,19 +1,67 @@
-var Q = require('q');
+var Client = require('uptime-robot');
+
 var id = 'summit-view-uptime-robot';
-var config, summit;
+var config = {}, summit, client, monitors = [], timeout;
+
+var scheduleUpdate = function() {
+    var lowest = 300;
+
+    for (var i = 0; i < monitors.length; i++) {
+        var interval = ( monitors[i].interval && parseInt(monitors[i].interval) ) ? parseInt(monitors[i].interval) : 300;
+        lowest = ( interval < lowest ) ? intervak : lowest;
+    }
+
+    timeout = setTimeout(updateMonitors, lowest * 1000);
+};
+
+var updateMonitors = function() {
+    if( client ) {
+        client.getMonitors()
+            .then(function(ms) {
+                monitors = ms;
+                summit.io.emit('monitors', monitors);
+                scheduleUpdate();
+            });
+    }
+    else {
+        monitors = [];
+        summit.io.emit('monitors', monitors);
+        clearTimeout(timeout);
+    }
+};
+
 
 module.exports = function(s) {
     summit = s;
 
     // emit the profiles on new connection
     summit.io.on('connection', function() {
-        var stuff = {};
-        summit.io.emit('event', stuff);
+        summit.io.emit('monitors', monitors);
     });
 
-    return {
-        id: id,
-    };
+
+    return summit.settings()
+        .then(function(settings) {
+            settings = settings || {};
+
+            if( !config.apiKey ) {
+                summit.registerSetting({
+                    name: 'apikey',
+                    label: 'API Key',
+                    type: 'text',
+                    value: settings.apikey || '',
+                });
+            }
+
+            if( config.apiKey || settings.apikey ) {
+                client = new Client( config.apiKey || settings.apikey );
+                updateMonitors();
+            }
+
+            return {
+                id: id,
+            };
+        });
 };
 
 module.exports.id = id;
@@ -22,4 +70,18 @@ module.exports.client = __dirname + '/lib/client.js';
 
 module.exports.style = __dirname + '/public/style.css';
 
-module.exports.onSettings = function(settings) {};
+module.exports.onSettings = function(settings) {
+    if( settings.apikey ) {
+        client = new Client( settings.apikey );
+    }
+    else {
+        client = false;
+    }
+
+    updateMonitors();
+};
+
+module.exports.init = function(cfg) {
+    config = cfg;
+    return module.exports;
+};
